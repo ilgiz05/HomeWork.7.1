a5ac54ae73218f095604c7586fc02dab5ea020ac
ilgiz05 <zaydamustafaeva@gmail.com>
2021-11-29 15:59:06 +0600

    Initial commit

---
+++ src/com/company/Warrior.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Warrior extends Hero{
+    public void applySuperAbility(String superAbilityTipy) {
+        System.out.println("Warrior применил способность АЛЛАКИ КОДЖИ");
+    }
+}
+
